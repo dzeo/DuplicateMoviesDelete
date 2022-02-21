5d90844867720c720c8a130be51f33cf3eb67337
Ezegwu Joseph <39636813+dzeo@users.noreply.github.com>
2022-02-21 03:44:21 +0100

    Create formats.py
    containing the video formats

---
+++ formats.py
@@ -0,0 +1,5 @@
+
+video_formats = (".mp4", ".3gp", ".ogg", ".wmv", ".webm", ".flv", ".avi", ".hdv", ".ts", ".mkv", ".srt", ".vob", ".ogv", ".gif", 
+        ".mng", ".MTS", ".M2TS", ".TS", ".mov",".qt", ".yuv", ".rm", ".rmvb", ".amv", ".asf", ".m4p", ".m4v",
+        ".mpg", ".mp2", ".mpeg", ".mpv", ".mpe", ".m2v", ".svi", ".3g2", ".mxf", ".nsv", ".f4v", ".f4p", ".f4a", ".f4b")
+
